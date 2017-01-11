99956b064b420b84cfaccb32ae42c3756f35001e
adolpheM <adojoe25@gmail.com>
2017-01-11 14:37:34 +0200

    change made on what of my items

---
+++ store_item.rb
@@ -0,0 +1,10 @@
+car = {"name" => "Bugatti", "color" => "black", "price" => 300}
+bike = {name:"Honda", color: "red", price: "50" }
+airplane = {"name" => "Boing", "color" => "white", "price" => 1000 }
+
+
+
+
+
+
+
