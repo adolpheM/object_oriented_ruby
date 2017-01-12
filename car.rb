ad8f502ffac37660e7a8ad94e7fcf2e79e2faf8a
adolpheM <adojoe25@gmail.com>
2017-01-12 11:20:19 +0200

    refactorized codes to simplify the work

--- car.rb
+++ car.rb
@@ -1,30 +1,17 @@
 class Car
 
+	attr_reader :name, :color, :price
+	attr_writer :price
+
 	def initialize(set_name, set_color, set_price)
 		@name = set_name
 		@color = set_color
 		@price = set_price
 
-	end 
-
-	def name
-		@name
-	end
-
-	def color
-		@color
-	end 
-
-	def price
-		@price
-	end 
-
-	def price=(set_price)
-		@price = set_price
-	end 
+	end  
 
 	def info
-		"our #{color} #{name} cost #{price}"
+		"our #{color} #{name} costs $#{price}"
 	end 
 
 
@@ -33,4 +20,4 @@ end
 car = Car.new("Bugatti", "black", 2000000)
 car.price=(2500000)
 
-puts car.info
+puts car.info	
